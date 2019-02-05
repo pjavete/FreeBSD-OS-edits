49206f21f1a02195d66289ae496d769784ac0e9e
David Lung <dflung@ucsc.edu>
2019-02-05 15:09:03 -0800

    bugfix?

--- asgn2/kern_switch.c
+++ asgn2/kern_switch.c
@@ -45,7 +45,7 @@ __FBSDID("$FreeBSD: releng/11.2/sys/kern/kern_switch.c 327481 2018-01-02 00:14:4
 
 #include <machine/cpu.h>
 
-#include <sys/libkern/random.c>
+#include <libkern/random.c>
 
 /* Uncomment this to enable logging of critical_enter/exit. */
 #if 0
