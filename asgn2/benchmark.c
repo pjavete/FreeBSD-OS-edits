1924b4b697a8f198547e27fa0977ac055d910345
David Lung <dflung@ucsc.edu>
2019-02-07 23:30:16 -0800

    asdf

--- asgn2/benchmark.c
+++ asgn2/benchmark.c
@@ -7,13 +7,16 @@
 int main(int argc, char *argv[]){
 
     if (argv[1] == NULL){
-        printf("Usage: benchmark <nice>");
+        printf("Usage: benchmark <nice>\n");
         exit(0);
     }
 
     int nice = atoi(argv[1]);
     if (nice >= -20 && nice <= 19){
         setpriority(PRIO_PROCESS, 0, nice);
+    } else {
+        printf("Error: Nice value out of range\n");
+        exit(0);
     }
     
     for(int i = 0; i < 1000000; i++){
