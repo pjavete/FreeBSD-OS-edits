ce0c2533002129a9f2258e84975e1ee146a0bb36
Payton <paytonjavete@sbcglobal.net>
2019-02-21 19:14:06 -0800

    changed pageout_upadte_period to 1 to try and change time between scans

--- asgn3/vm_pageout.c
+++ asgn3/vm_pageout.c
@@ -176,7 +176,7 @@ SYSCTL_INT(_vm, OID_AUTO, pageout_wakeup_thresh,
 	CTLFLAG_RWTUN, &vm_pageout_wakeup_thresh, 0,
 	"free page threshold for waking up the pageout daemon");
 
-SYSCTL_INT(_vm, OID_AUTO, pageout_update_period,
+SYSCTL_INT(_vm, OID_AUTO, 1,
 	CTLFLAG_RWTUN, &vm_pageout_update_period, 0,
 	"Maximum active LRU update period");
   
