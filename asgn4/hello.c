05cfdc0c979854824b0e9c67e6b006ac7f9ca8fc
David Lung <dflung@ucsc.edu>
2019-03-11 21:04:25 -0700

    code

--- asgn4/hello.c
+++ asgn4/hello.c
@@ -58,7 +58,7 @@ static int getSize(struct metadata md){
 
 static int hello_getattr(const char *path, struct stat *stbuf)
 {
-	printf("getattr\n");
+	printf("getattr for %s\n", path);
 	int res = 0;
 	int file_found = 0;
 	int block_num;
@@ -113,7 +113,7 @@ static int hello_getattr(const char *path, struct stat *stbuf)
 static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 						 off_t offset, struct fuse_file_info *fi)
 {
-	printf("readdir\n");
+	printf("readdir for %s\n", path);
 	(void)offset;
 	(void)fi;
 
